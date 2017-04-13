5331a4410bb6778f75b785c35a3e089cd47b51db
Shinya Kumgai <roomful.rooms@gmail.com>
2017-04-13 22:06:35 +0900

    Implement main activity layout

--- android/app/src/main/java/com/droibit/hello/firebase/MainActivity.kt
+++ android/app/src/main/java/com/droibit/hello/firebase/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.droibit.hello.firebase
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.droibit.hello.firebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
