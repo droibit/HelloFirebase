2058b24856318c4407ad38ec06ad77938c138084
Shinya Kumagai <roomful.rooms@gmail.com>
2017-04-21 16:59:03 +0900

    Add Post class

---
+++ android/app/src/main/java/com/droibit/hello/firebase/model/Post.kt
@@ -0,0 +1,24 @@
+package com.droibit.hello.firebase.model
+
+import com.google.firebase.database.Exclude
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class Post(
+        var uid: String,
+        var author: String,
+        var title: String,
+        var body: String,
+        var starCount: Int,
+        val stars: Map<String, Boolean> = hashMapOf()) {
+
+    @Exclude
+    fun toMap() = hashMapOf(
+            Post::uid.name to uid,
+            Post::author.name to author,
+            Post::title.name to title,
+            Post::body.name to body,
+            Post::starCount.name to starCount,
+            Post::stars.name to stars
+    )
+}
