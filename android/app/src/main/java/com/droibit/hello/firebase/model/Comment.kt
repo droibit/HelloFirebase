32b8083353435c0069f2689edd28668d3aa417bc
Shinya Kumagai <roomful.rooms@gmail.com>
2017-04-21 17:58:08 +0900

    Add Comment class

---
+++ android/app/src/main/java/com/droibit/hello/firebase/model/Comment.kt
@@ -0,0 +1,6 @@
+package com.droibit.hello.firebase.model
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class Comment(var uid: String, var author: String, var text: String)
