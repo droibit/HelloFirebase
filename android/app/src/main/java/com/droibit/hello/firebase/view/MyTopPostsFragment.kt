3a92f5249470f27e81e15351ecb02e64180feecc
Shinya Kumgai <roomful.rooms@gmail.com>
2017-04-28 23:06:48 +0900

    Implement MyTopPostsFragment

--- android/app/src/main/java/com/droibit/hello/firebase/view/MyTopPostsFragment.kt
+++ android/app/src/main/java/com/droibit/hello/firebase/view/MyTopPostsFragment.kt
@@ -1,6 +1,15 @@
 package com.droibit.hello.firebase.view
 
-import android.support.v4.app.Fragment
+import com.droibit.hello.firebase.PostListFragment
+import com.droibit.hello.firebase.model.Post
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.Query
 
-class MyTopPostsFragment : Fragment() {
+class MyTopPostsFragment : PostListFragment() {
+
+    override fun createQuery(databaseRef: DatabaseReference): Query {
+        return databaseRef.child("user-posts")
+                .child(currentUser.uid)
+                .orderByChild(Post::starCount.name)
+    }
 }
