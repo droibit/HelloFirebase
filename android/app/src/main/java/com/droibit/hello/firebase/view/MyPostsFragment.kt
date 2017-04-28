07cf0c7dd7ae7b8cee4239f7d86c416f7b0412e1
Shinya Kumgai <roomful.rooms@gmail.com>
2017-04-28 23:01:55 +0900

    Implement MyPostsFragment

--- android/app/src/main/java/com/droibit/hello/firebase/view/MyPostsFragment.kt
+++ android/app/src/main/java/com/droibit/hello/firebase/view/MyPostsFragment.kt
@@ -1,6 +1,12 @@
 package com.droibit.hello.firebase.view
 
-import android.support.v4.app.Fragment
+import com.droibit.hello.firebase.PostListFragment
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.Query
 
-class MyPostsFragment : Fragment() {
+class MyPostsFragment : PostListFragment() {
+
+    override fun createQuery(databaseRef: DatabaseReference): Query {
+        return databaseRef.child("user-posts").child(currentUser.uid)
+    }
 }
