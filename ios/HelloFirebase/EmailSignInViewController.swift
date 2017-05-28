9b1e3dbf10ae8fa85bfa8a8f1ac09185f787ea41
Shinya Kumgai <roomful.rooms@gmail.com>
2017-05-28 21:39:52 +0900

    Added signIn/Up with Email

--- ios/HelloFirebase/EmailSignInViewController.swift
+++ ios/HelloFirebase/EmailSignInViewController.swift
@@ -7,20 +7,40 @@
 //
 
 import UIKit
+import Firebase
+import SVProgressHUD
 
 class EmailSignInViewController: UIViewController {
     
+    typealias Task = (String, String, @escaping FirebaseAuth.AuthResultCallback) -> Void
+    
     enum SignInType: String {
-        case SignUp = "SignUp"
-        case SignIn = "SignIn"
+        case SignUp = "Sign Up"
+        case SignIn = "Sign In"
     }
     
-    var signInType: SignInType!
+    var signInType: SignInType! = nil {
+        willSet {
+            switch newValue! {
+            case .SignUp:
+                task = self.signUp
+            case .SignIn:
+                task = self.signIn
+            }
+        }
+    }
     
     var completed = false
+    
+    private var task: Task!
+    
+    fileprivate let auth = Auth.auth()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let navigationViewController = parent as! UINavigationController
+        navigationViewController.navigationBar.topItem?.title = signInType.rawValue
 
         // Do any additional setup after loading the view.
     }
@@ -40,5 +60,17 @@ class EmailSignInViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+}
 
+extension EmailSignInViewController {
+    
+    func signUp(email: String, password: String, completion: @escaping FirebaseAuth.AuthResultCallback) {
+        auth.createUser(withEmail: email, password: password, completion: completion)
+    }
+    
+    func signIn(email: String, password: String, completion: @escaping FirebaseAuth.AuthResultCallback) {
+        auth.signIn(withEmail: email, password: password, completion: completion)
+    }
+    
+    
 }
